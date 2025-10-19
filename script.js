fa4bc3ae1a0f429cf0a361889c2772e3118bfb72
mohamedAttefi <attefimohamed0@gmail.com>
2025-10-19 20:11:05 +0100

    hello

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function submit() {
-    
+    let x = document.getElementById("number").value;
+    let result = document.getElementById("result");
+    function dooble(a) {
+        return a * 2;
+    }
+    let z = dooble(x);
+    result.textContent = z;
 
 }
