89b1b9e5728f7fe3d6476a1c677505cc038814e9
mohamedAttefi <attefimohamed0@gmail.com>
2025-10-19 20:14:06 +0100

    stash

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function submit() {
-    let x = document.getElementById("number").value;
-    let result = document.getElementById("result");
-    function dooble(a) {
-        return a * 2;
-    }
-    let y = dooble(x);
-    result.textContent = y;
+    
 
 }
