26da68c8938124c009a43c9e27c07fbf23805b40
mohamedAttefi <attefimohamed0@gmail.com>
2025-10-20 14:25:35 +0100

    work

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let x = 14;
-function dooble(a){
-    return a*2;
+function submit() {
+    let x = document.getElementById("number").value;
+    let result = document.getElementById("result");
+    function dooble(a) {
+        return a * 2;
+    }
+    let z = dooble(x);
+    result.textContent = z;
+    let w = 3;
+    lkekekk
 }
-let y = dooble(x);
-console.log(y);
